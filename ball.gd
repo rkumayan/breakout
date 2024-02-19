bd8e4eb2a2d25e7a18401d425c3e75dd4329ced2
rkumayan <kumayanrahul01@gmail.com>
2024-02-19 19:08:48 +0530

    added collision for player and ball

--- ball.gd
+++ ball.gd
@@ -1,14 +1,27 @@
 extends Area2D
 
-var add_x = 1
-var add_y = 1
+var add_x = -10
+var add_y = 10
 func _physics_process(delta):
 	position += Vector2(add_x,add_y) 
 	pass
 
 
 func _on_body_entered(body):
-	add_y = -add_y
-	print('body entered')
+	#player entered
+	if( str(body) == "player:<CharacterBody2D#27497858266>"):
+		add_y = -add_y
+	#collided with right or left wall
+	if( str(body) == "right:<StaticBody2D#27464303832>" or str(body) ==  "left:<StaticBody2D#27430749398>"):
+		add_x = -add_x
+	
+	#collided with top or bottom wall
+	if( str(body) == "top:<StaticBody2D#27363640524>" or str(body) == "bottom:<StaticBody2D#27397194952>"):
+		add_y = -add_y
+	
+	
+		
+	print(body)
+	
 	pass
 	
--- player.gd
+++ player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+const speed = 300
 func _physics_process(delta):
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if( Input.is_action_pressed("ui_left")):
-		position.x -= 100 * delta
+		position.x -= speed * delta
 	elif( Input.is_action_pressed("ui_right")):
-		position.x += 100 * delta
+		position.x += speed * delta
 
 	
